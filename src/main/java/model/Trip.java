dedea961275e3eebadc13649fb3479a98ac44016
Jakub Fik <fikkuba@gmail.com>
2021-04-12 14:57:35 +0200

    Trip class modification + GitIgnore

--- src/main/java/model/Trip.java
+++ src/main/java/model/Trip.java
@@ -2,6 +2,7 @@ package model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Trip {
     private LocalDate startOfTrip;
@@ -39,5 +40,28 @@ public class Trip {
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Trip trip = (Trip) o;
+        return Objects.equals(startOfTrip, trip.startOfTrip) && Objects.equals(endOfTrip, trip.endOfTrip) && Objects.equals(destiny, trip.destiny) && Objects.equals(price, trip.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startOfTrip, endOfTrip, destiny, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Trip{" +
+                "startOfTrip=" + startOfTrip +
+                ", endOfTrip=" + endOfTrip +
+                ", destiny='" + destiny + '\'' +
+                ", price=" + this.getPrice() +
+                '}';
     }
 }
